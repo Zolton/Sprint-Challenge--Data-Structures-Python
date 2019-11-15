94f84dfecd330f6aaae12f491cb6ec118da3f5a4
Zolton <gmzesq@gmail.com>
2019-11-15 11:32:24 -0500

    Ring Buffer done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,41 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    ringList = self.storage
+    print("index 0 is: ", ringList[0])
+    print("index 1 is: ", ringList[1])
+    if self.current == self.capacity:
+      self.current = 0
+    ringList[self.current] = item
+    print("Current index is: ", self.current)
+    self.current += 1
 
   def get(self):
-    pass
+    cleanList = [i for i in self.storage if i != None]
+    
+    return cleanList
+
+""" python
+buffer = RingBuffer(3)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+buffer.get()   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+buffer.get()   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+maybe some kind of counter, reset at 3, that keeps track of which
+should be overwritten?
+
+buffer.get()   # should return ['d', 'e', 'f']
+"""
